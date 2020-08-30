7805c62caeb39a56c2ebf70550a4f5d3427f0f3d
nhope <16958187+nhope123@users.noreply.github.com>
2020-08-29 23:40:07 -0400

    update

--- src/drums.js
+++ src/drums.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import './index.css';
 
+// First audio list
+const fx1 = {
+  qAudio: ['Heater-1.mp3','Heater 1'],
+  wAudio: Heater-2.mp3,Heater-3.mp3,Heater-4_1.mp3,Heater-6.mp3, Dsc_Oh.mp3', Kick_n_Hat.mp3, Chord_2.mp3, Chord_3.mp3']
+}
+
+// Second audio list
+
+
+
 // Drum machine Button Component emplimenting the button functionality
 class Buttons extends React.Component{
   constructor(props){
@@ -26,7 +36,7 @@ class Buttons extends React.Component{
         <button type='button' className='drum-pad'  onClick={this.playAudio} value='qButton'>
           <audio id='qButton' controls >
           {/* // BUG: fix source */}
-            <source src={this.qSrc} type='audio/mp3' />
+            <source src='./audio/Cev_H2.mp3' type='audio/mp3' />
             Your browser does not support the audio element.
           </audio>
           Q
