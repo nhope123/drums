9d021e16d5f3a246c4b71bbaee8799091272510b
nhope <16958187+nhope123@users.noreply.github.com>
2020-09-02 14:19:18 -0400

    Updates

---
+++ precache-manifest.222df4a5712338ef67ce718e007535ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cc34a515f99988d4c4b22f0a2ccf708",
+    "url": "/drums/index.html"
+  },
+  {
+    "revision": "88ecc5e94bef3b277236",
+    "url": "/drums/static/css/main.8abbc805.chunk.css"
+  },
+  {
+    "revision": "af83c9769cecb8a7952b",
+    "url": "/drums/static/js/2.f1d84701.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/drums/static/js/2.f1d84701.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88ecc5e94bef3b277236",
+    "url": "/drums/static/js/main.de572d3e.chunk.js"
+  },
+  {
+    "revision": "aa59b7696db63f717b0a",
+    "url": "/drums/static/js/runtime-main.5b5778aa.js"
+  }
+]);
